8b5838920c388ad26bba650e645a33a03b8ace75
Shivani Kushwaha <shivi.ymca@gmail.com>
2022-03-01 14:16:32 +0530

    Create Day2_leetcode_338.cpp

---
+++ Day2_leetcode_338.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i = 0; i<=n; i++){
+            int p = __builtin_popcount(i);
+            ans.push_back(p);
+        }
+        return ans;
+    }
+};
